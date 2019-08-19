8425ddbc07c87c116cd3ccf8334f35a83b257684
AbramReda <AbramReda@github.com>
2019-08-19 19:39:21 +0200

    adding slider with bxSlider plugin

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
-
-
-
 $(function(){
 	'use strict';
 	
 	
 	//Adjust header hieght
 	var myHeader =$(".header"); //cashing header class
+	var mySlider =$('.slider'); //caching slider
+	
 	myHeader.height($(window).height());
 	
 	$(window).resize(function(){
 		myHeader.height($(window).height());
+		//make slider div in center of ccreen		
+		mySlider.each(function(){
+			$(this).css('paddingTop',($(window).height()-$('.slider div').height()) /2);
+		});
 	});
 	
 	
@@ -18,6 +21,18 @@ $(function(){
 	
 	$('.links li').click(function(){
 		$(this).addClass("active").siblings().removeClass("active");
+	});
+	
+	
+	//make slider div in center of ccreen
+	
+	mySlider.each(function(){
+		$(this).css('paddingTop',($(window).height()-$(this).height()) /2);
+	});
+	
+	// Initialize the slider
+	mySlider.bxSlider({
+		pager:false
 	});
 	
 });
